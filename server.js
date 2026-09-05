9ecf1d491d7a95c84b4f30958f67a6626270d525
guofoon-tech <guofoon@gmail.com>
2026-09-05 14:42:06 +0700

    Serve PROMO HUB frontend

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = require('./api/index.js');
+
+// Serve the PROMO HUB web app from /public.
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 module.exports = app;
